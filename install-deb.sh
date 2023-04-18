d4b003611374d9ee4cf652dd2ee17ea418a52e78
virskl <andreas.burnickl@vector.com>
2023-04-18 14:46:01 +0200

    Fix build-deb-package.sh

--- install-deb.sh
+++ install-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 dpkg -r --force-depends raspberrypi-kernel
-dpkg -i ./deb-package/raspberrypi-kernel_1.20230405-1_arm64.deb
+dpkg -i ./raspberrypi-kernel_1.20230405-1_arm64.deb
 
 #apt-mark hold raspberrypi-kernel
